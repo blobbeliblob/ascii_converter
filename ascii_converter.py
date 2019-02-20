29247b83837b19983eedd2c78f7193264a8568b1
Camilo Hernández <camilo.hernandeznyreen@aalto.fi>
2019-02-20 21:59:44 +0200

    Update ascii_converter.py

--- ascii_converter.py
+++ ascii_converter.py
@@ -10,6 +10,7 @@ def main():
     font = "Fonts/arial.ttf"
     fontSize = 8
     letter = "@"
+	method = "random"
     threshold = 100
     for file in os.listdir(src_dir):
         if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
@@ -20,7 +21,7 @@ def main():
             img.setDefaultLetter(letter)
             img.monochrome(threshold)
             img.resize(img.getWidth()//scale_factor, img.getHeight()//scale_factor)
-            img.initializeMatrix("random")
+            img.initializeMatrix(method)
             img.saveMatrix(target_dir + "/" + file.replace(".png","").replace(".jpg","").replace(".jpeg","") + target_extra + ".png", format="img")
 
 main()
